513a4f4c825c26822532b7bd3c7bb855f858392a
joe miller <joeym@joeym.net>
2013-07-11 07:14:35 -0700

    reflect recent changes in readme

--- README.markdown
+++ README.markdown
@@ -1,5 +1,5 @@
 dns_compare.py
-=====================================================================================================
+==============
 Compare data from a BIND zone file to data returned by an authoritative DNS server.
 
 Purpose
@@ -17,19 +17,35 @@ Windows 2000 DNS and GoDaddy DNS (which both export BIND zone files) into Amazon
 Route53 DNS service.  With this tool, I could confidently prove that all records
 properly imported into Route53 before changing the whois records for each domain.
 
+UPDATED (7/11/2013): Cleaned up the script a bit and refacdtored a few things while migrating
+a set of domains from Ziergo to Dyn.
+
 Example Usage:
 --------------
 Basic operation:
-	``
+	```
     $ dns_compare.py -z example.com --file example.com.zone --server 10.1.1.1
-    ....X...X..done
+    ............................................X
+    (MIS-MATCH) query: nss4.example.com.
+     Expected:  300 IN A 142.229.40.28
+     Received:  900 IN A 142.229.40.28
+    ...X
+    (MIS-MATCH) query: www.example.com.
+     Expected:  200 IN A 91.139.129.128
+     Received:  900 IN A 91.139.129.128
+    X
+    (MIS-MATCH) query: www.example.com.
+     Expected:  300 IN AAAA 2001:4800:1078:2256:78C8:1542:FF04:6BCB
+     Received:  900 IN AAAA 2001:4800:1078:2256:78c8:1542:ff04:6bcb
+    ......................done
+    
     Results:
-    Matches:      9
-    Mis-matches:  2
-	``
+    Matches:      69
+    Mis-matches:  3
+    ```
 
 Verbose:
-	``
+	```
     $ dns_compare.py -z example.com --file example.com.zone --server 10.1.1.1 --verbose
 	----
 	(Match) query: www.example.com. ...
@@ -39,14 +55,14 @@ Verbose:
 	(MIS-MATCH) query: example.com. ...
 	Expected:  60 IN A 10.0.0.1
 	Got     :  60 IN A 10.0.0.20
-	``
-
-By default, TTL values will _not_ be compared.  Specify the --ttl option to
-enable TTL comparison.
+	```
 
 By default, SOA and NS records are ignored because these records are likely
-to change when migrating a zone between DNS services..  Specify --soa or --ns option,
+to change when migrating a zone between DNS services..  Specify `--soa` or `--ns` option,
 respectively, to enable checking of SOA and NS records.
+
+Comparing TTLs can be disabled with `-t` option. This is useful when transferring DNS to a provider
+that offers only specific TTL values.
 
 TODO:
 -----
@@ -54,4 +70,4 @@ TODO:
 
 Author
 ------
-Joe Miller (http://github.com/joemiller) (http://www.joeym.net)
+Joe Miller (http://github.com/joemiller) (http://joemiller.me) (https://twitter.com/miller_joe)
--- dns_compare.py
+++ dns_compare.py
@@ -34,100 +34,90 @@ import sys, socket
 from pprint import pprint
 
 try:
-	import dns.resolver, dns.zone
-	from dns.exception import DNSException
-	from dns.rdataclass import *
-	from dns.rdatatype import *
+    import dns.resolver, dns.zone
+    from dns.exception import DNSException
+    from dns.rdataclass import *
+    from dns.rdatatype import *
 except ImportError:
-	print "Please install dnspython:"
-	print "$ sudo easy_install dnspython"
-	sys.exit(-1)
+    print "Please install dnspython:"
+    print "$ sudo pip install dnspython"
+    sys.exit(-1)
 
-parser = OptionParser()
-# required options
-parser.add_option("-z", "--zone", dest="zone", metavar="DOMAIN",
-					help="name of the domain we're checking (eg: domain.com)")
-parser.add_option("-f", "--file", dest="zonefile", metavar="FILE",
-					help="zone file to load records from")
-parser.add_option("-s", "--server", dest="nameserver", metavar="HOST",
-					help="DNS server to compare zone file against")
-# optional .. options
-parser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=False,
-					help="print detailed results of each action")				
-parser.add_option("-a","--soa", dest="compare_soa", action="store_true", default=False,
-					help="compare SOA records (default: false)")					
-parser.add_option("-n","--ns", dest="compare_ns", action="store_true", default=False,
-					help="compare NS records (default: false)")					
-parser.add_option("-t","--ttl", dest="compare_ttl", action="store_true", default=False,
-					help="compare TTL values (default: false)")					
-					
-(opts, remaining_args) = parser.parse_args()
 
-# check for required options, since optparse doesn't support required options
-if opts.zone == None or opts.zonefile == None or opts.nameserver == None:
-	print "Error: required arguments: --zone, --file, --server (or --help)"
-	sys.exit(-1)
+if __name__ == '__main__':
+    parser = OptionParser()
+    # required options
+    parser.add_option("-z", "--zone", dest="zone", metavar="DOMAIN",
+                        help="name of the domain we're checking (eg: domain.com)")
+    parser.add_option("-f", "--file", dest="zonefile", metavar="FILE",
+                        help="zone file to load records from")
+    parser.add_option("-s", "--server", dest="nameserver", metavar="HOST",
+                        help="DNS server to compare zone file against")
+    # optional ... options
+    parser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=False,
+                        help="print detailed results of each action")
+    parser.add_option("-a","--soa", dest="compare_soa", action="store_true", default=False,
+                        help="compare SOA records (default: false)")
+    parser.add_option("-n","--ns", dest="compare_ns", action="store_true", default=False,
+                        help="compare NS records (default: false)")
+    parser.add_option("-t","--ttl", dest="compare_ttl", action="store_false", default=True,
+                        help="compare TTL values (default: true)")
+    (opts, remaining_args) = parser.parse_args()
 
-z = dns.zone.from_file(opts.zonefile, origin=opts.zone, relativize=False)
+    # check for required options, since optparse doesn't support required options
+    if opts.zone == None or opts.zonefile == None or opts.nameserver == None:
+        print "Error: required arguments: --zone, --file, --server (or --help)"
+        sys.exit(-1)
 
-r = dns.resolver.Resolver(configure=False)
-r.nameservers = [socket.gethostbyname(opts.nameserver)]
+    z = dns.zone.from_file(opts.zonefile, origin=opts.zone, relativize=False)
 
-matches=0
-mismatches=0
-for (name, rdataset) in z.iterate_rdatasets():
-	if rdataset.rdtype == SOA and opts.compare_soa == False:
-		continue
-	if rdataset.rdtype == NS and opts.compare_ns == False:
-		continue
-			
-	match = False
-	result = None
-	try:
-		ans = r.query(name, rdataset.rdtype, rdataset.rdclass)
-		result = ans.rrset.to_rdataset()
-		if result == rdataset:
-			if opts.compare_ttl:
-				if result.ttl == rdataset.ttl:				
-					match = True
-			else:
-				match = True
-	except DNSException, e:
-		pass
-		
-	if opts.verbose:
-		description = ''
-		if match:
-			description = 'Match'
-		else:
-			description = 'MIS-MATCH'
-		print "----"
-		print "(%s) query: %s ..." % (description, name)
-		print "Expected: ", rdataset
-		print "Received: ", result
-	
-	if match:
-		if opts.verbose == False:
-			sys.stdout.write('.')
-			sys.stdout.flush()
-		matches += 1
-	else:
-		if opts.verbose == False:
-			sys.stdout.write('X')
-			sys.stdout.flush()
-		mismatches += 1
-print "done"
+    r = dns.resolver.Resolver(configure=False)
+    r.nameservers = [socket.gethostbyname(opts.nameserver)]
 
-print "\nResults:"
-print "Matches:     ", matches
-print "Mis-matches: ", mismatches
+    matches=0
+    mismatches=0
+    for (name, rdataset) in z.iterate_rdatasets():
+        if rdataset.rdtype == SOA and not opts.compare_soa:
+            continue
+        if rdataset.rdtype == NS and not opts.compare_ns:
+            continue
 
-if opts.verbose == False and mismatches > 0:
-	print " (re-run with --verbose to see details of the mis-matches )"
-	
-def lookup_dns_from_local_server(addr):
-	iplist = []
-	ans = dns.resolver.query(addr)
-	for ip in ans.rrset.items:
-		iplist.append(ip.to_text())
-	return iplist
+        match = False
+        result = None
+        try:
+            ans = r.query(name, rdataset.rdtype, rdataset.rdclass)
+            result = ans.rrset.to_rdataset()
+            if result == rdataset:
+                if opts.compare_ttl:
+                    if result.ttl == rdataset.ttl:
+                        match = True
+                else:
+                    match = True
+        except DNSException, e:
+            pass
+
+        if opts.verbose:
+            description = 'Match' if match else 'MIS-MATCH'
+            print "----"
+            print "(%s) query: %s" % (description, name)
+            print "Expected: ", rdataset
+            print "Received: ", result
+
+        if match:
+            if not opts.verbose:
+                sys.stdout.write('.')
+                sys.stdout.flush()
+            matches += 1
+        else:
+            if not opts.verbose:
+                sys.stdout.write('X')
+                print "\n(MIS-MATCH) query: %s" % name
+                print " Expected: ", rdataset
+                print " Received: ", result
+                sys.stdout.flush()
+            mismatches += 1
+    print "done"
+
+    print "\nResults:"
+    print "Matches:     ", matches
+    print "Mis-matches: ", mismatches
