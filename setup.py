e2e8e8450cec5d5c3478a2ad2dcce80b02ff3e8f
Jean-Francois Chevrette <jfchevrette@gmail.com>
2020-08-19 09:41:29 -0400

    update to work on python3

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='dns_compare',
     version='0.0.3',
-    author='Joe Miller',
-    author_email='joeym@joeym.net',
+    author='Jean-Francois Chevrette',
+    author_email='jfchevrette@gmail.com',
     scripts=['dns_compare'],
-    url='https://github.com/joemiller/dns_compare',
+    url='https://github.com/jfchevrette/dns_compare',
     license='LICENSE',
     description="Test DNS servers",
-    long_description=open('README.markdown').read()
+    long_description=open('README.markdown').read(),
+    python_requires='>=3.6',
+    install_requires=[
+        'dnspython ~= 2.0',
+    ]
 )
